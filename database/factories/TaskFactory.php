afd261b7daf541462c0f9feda8b5898f40ce5153
wintan1418 <wintan1418@gmail.com>
2025-11-24 23:23:55 +0100

    Add database factories and seeders

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->paragraph(),
+            'completed' => fake()->boolean(30), // 30% chance of being completed
+            'user_id' => \App\Models\User::factory(),
+        ];
+    }
+}
