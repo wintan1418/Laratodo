25dd03e8f97c4035f4cdee971bffdb8b1080df81
wintan1418 <wintan1418@gmail.com>
2025-11-24 23:37:58 +0100

    Add weather widget with fallback message when API unavailable

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -47,6 +47,20 @@
                         </div>
                     </div>
                 </div>
+            @else
+                <div class="mb-4 bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg p-4">
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-yellow-600 dark:text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                        </svg>
+                        <div>
+                            <p class="text-sm text-yellow-800 dark:text-yellow-200">
+                                <strong>Weather unavailable:</strong> Please check your OpenWeatherMap API key in <code>.env</code> file. 
+                                <a href="https://openweathermap.org/api" target="_blank" class="underline">Get a free API key here</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             @endif
 
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
